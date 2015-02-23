5852ad48378f7af3fa903e7b5667081dbc4809dc
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-02-23 10:59:13 +0100

    completely remove modal for manage publish

--- public/res/eventMgr.js
+++ public/res/eventMgr.js
@@ -12,7 +12,6 @@ define([
 	"extensions/partialRendering",
 	"extensions/buttonMarkdownSyntax",
 	"extensions/dialogAbout",
-	"extensions/dialogManagePublication",
 	"extensions/dialogManageSynchronization",
 	"extensions/dialogManageSharing",
 	"extensions/dialogOpenHarddrive",
